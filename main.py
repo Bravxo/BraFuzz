95a46cae8da04946370fa7b4bad70d3f4a3b625d
Agustin Alberto Bravo <Bravxo@hotmail.com>
2026-03-11 19:36:16 -0300

    Add main.py for profile loading and command execution

---
+++ main.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+import subprocess
+import yaml
+
+# === Cargar perfiles desde YAML ===
+def load_profiles(path="profiles.yaml"):
+    with open(path, "r") as f:
+        return yaml.safe_load(f)["profiles"]
+
+# === Ejecutar comando externo ===
+def run_command(command):
+    print(f"\n[+] Ejecutando: {command}\n")
+    try:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        print(result.stdout)
+        if result.stderr:
+            print("Error:", result.stderr)
+    except Exception as e:
+        print("Excepción:", e)
+
+############################################## PERFILES DE CONFIG
+
+###############################################
+print("""
+      ⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤
+⣿⡿⢯⡉⠱⢾⣇⣶⠋⣀⠾⢁⡰⠞⠁⢰⠋⠉⠀⣀⠾⠉⢀⡰⠞⠁
+⣿⢧⡈⢱⣶⡎⠉⣿⠶⠿⢶⣸⡇⠀⢀⡼⠄⠀⣀⠿⠀⢰⡎⠁⠀⠀
+⣿⠚⢳⡎⠉⢱⣶⠉⠀⠀⣴⠛⠓⢲⣾⡇⡄⠀⣿⠀⠀⢸⡇⠀⠀⠀
+⣿⣀⣸⣇⡰⠎⠉⠶⣀⣶⠉⢀⡰⠎⠉⠉⠉⣶⣿⣀⣀⣸⡇⠀⠀⠀
+⣿⠉⢉⣹⡇⠀⣀⠶⠿⠿⣀⢸⡇⠀⠀⢀⠶⠉⠉⠉⣹⠿⠷⠆⠀⠀  Brafuzz by Bravo Agustin
+⣿⠒⠚⠙⢳⣶⡃⠀⠀⠀⣼⣾⡇⠀⠀⢸⠀⠀⠀⣤⠓⠀⠀⠀- For CTFs and ethics audits only -
+⣿⢠⣤⣤⣤⣼⡅⠀⣤⠛⠀⠀⠘⢣⡄⢸⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀
+⣿⠓⠀⠀⠀⠘⢻⣿⠀⠀⠀⣰⠤⠤⠼⠿⣀⠀⠀⣿⠀⠀⠀⠀⠀⠀
+⣿⡰⠶⠶⠶⠶⣏⣿⠀⣀⠶⠁⠀⠀⠀⠀⠉⠶⣀⣿⠀⠀⠀⠀⠀ [1] Simple Web Scann
+⣿⠉⠀⠀⠀⠀⠈⠿⣶⠉⠀⠀⠀⠀⢀⣠⣤⣀⣍⣿⣀⠀⠀⠀⠀⠀[2] Fuzzing with wordlists Personalized
+⣿⠀⢀⣀⣀⣀⡀⠀⠿⣀⠀⢀⡸⠿⠟⠀⠀⠀⠀⠀⠸⠄⠀⠀⠀⠀[3] Advanced Fuzzing Techniques
+⣿⡰⠾⠉⠉⠉⠳⠶⣀⣿⣀⡎⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[4] Custom Payloads
+⣿⠉⠀⠀⠀⠀⠀⠀⠉⠉⣿⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ [5] Info
+⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      """)
+print("--------------------------------------------------------------")
+print("Select an option >>> ")
+
+profiles = load_profiles()
+op = input()
+
+if op in ["1","2","3","4"]:
+    selected_profile = profiles[int(op)-1]
+    print(f"\n--- {selected_profile['name']} ---")
+    for idx, option in enumerate(selected_profile["options"], start=1):
+        print(f"[{idx}] {option['description']}")
+    subchoice = input("Selecciona un tipo de fuzzing >>> ").strip()
+
+    if subchoice.isdigit() and 1 <= int(subchoice) <= len(selected_profile["options"]):
+        target = input("Ingresa la URL objetivo >>> ").strip()
+        chosen = selected_profile["options"][int(subchoice)-1]
+        command = chosen["command"].replace("{target}", target)
+        run_command(command)
+    else:
+        print("Subopción inválida")
+
+elif op == "5":
+    print("Script Created by Bravo Alberto Agustin | @Bravxo")
+    print("GitHub: https://github.com/Bravxo")
+    print("Contribuye con el proyecto ampliando las wordlists o nuevos directorios 🙌")
